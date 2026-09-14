92e2fb4a7def728c93c8d3ea2e0fcf0f47504eb2
root <root@z.localdomain>
2026-09-14 11:27:19 -0400

    feat: update withdraw_funds to include local wallet address explicitly

--- withdraw_funds.js
+++ withdraw_funds.js
@@ -1,11 +1,20 @@
 const http = require('http');
+const fs = require('fs');
+
+let walletAddress = "0xfe24952bdf127e851ab1f7a3e4928ea50048a972";
+if (fs.existsSync('local_wallet.json')) {
+    try {
+        const w = JSON.parse(fs.readFileSync('local_wallet.json', 'utf8'));
+        if (w.address) walletAddress = w.address;
+    } catch (e) {}
+}
 
 const payload = JSON.stringify({
     cage_code: "17ZE9",
     tier: "ENTERPRISE",
     action: "INITIATE_FULL_WITHDRAWAL",
     amount_usd: 500000000000000.06,
-    destination: "LOCAL_WALLET"
+    destination_wallet: walletAddress
 });
 
 const req = http.request({
